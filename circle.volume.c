8a42fc6f31e51f597daddad6de09c09991bca3d8
angad <angadreact01@gmail.com>
2021-12-15 09:15:09 -0800

    first commit

---
+++ circle.volume.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // PROGRAME TO CALCULATE VOLUME OF A CIRCLE
+    // WE KNOW THAT VOLUME OF A CIRCLE IS PI*R*R
+    // SO PI = 3.14 
+    // OUR FORMULA IS 3.14 * R * R
+    int r ;
+    int height;
+    float pi = 3.14;
+    printf("what is your radius \n");
+    scanf("%d", &r);
+    printf("what is your height \n");
+    scanf("%d", &height);
+    printf("your volume of a circle is %f \n", pi * r * r * height);
+    return 0;
+}
